584903f79144964660e3707a89e35a1bc7379b2d
fj <castelloiv@gmail.com>
2020-10-01 15:15:30 -0400

    More API work with google maps

--- src/components/zipform.js
+++ src/components/zipform.js
@@ -1,38 +1,60 @@
 import React, { useState } from "react";
-import { GoogleMap, DistanceMatrixService } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  DistanceMatrixService,
+  LoadScript,
+} from "@react-google-maps/api";
 import Styles from "./zipform.module.scss";
 
-const zips = () => {
+const Zips = () => {
   const [response, setResponse] = useState(null);
   const [travelMode, setTravelMode] = useState("driving");
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
+  const API_KEY = process.env.GOOGLE_API_KEY;
+
+  const containerStyle = {
+    width: "400px",
+    height: "400px",
+    margin: "0 auto",
+  };
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
   return (
     <div>
       <section className={Styles.myForm}>
-        <h1>Ayyyy</h1>
+        <h1>Zip code to zip code(s) driving distance(s)</h1>
+        <div className={Styles.flex}>
+          <div className={Styles.origin}>
+            <label htmlFor="ORIGIN">Origin Zip</label>
+            <br />
+            <input
+              id="ORIGIN"
+              className="formControl"
+              type="text"
+              useRef={origin}
+            />
+          </div>
+        </div>
       </section>
 
-      <div className="map-container">
-        <GoogleMap
-          id="map"
-          mapContainerStyle={mapContainerStyle}
-          zoom={14}
-          center={center}
-          options={options}
-        >
-          <DistanceMatrixService
-            options={{
-              destinations: this.state.destination,
-              origins: this.state.origin,
-              travelMode: this.state.travelMode,
-            }}
-            callback={this.distanceCallback}
-          />
-        </GoogleMap>
+      <div className="mapCon">
+        <LoadScript googleMapsApiKey={API_KEY}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+          >
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        </LoadScript>
       </div>
     </div>
   );
 };
 
-export default zips;
+export default Zips;
