083215ff13c48d88272e1c5c7ad3469800afa1e3
fj <castelloiv@gmail.com>
2020-10-03 15:38:48 -0400

    Really?

---
+++ src/components/map/Map.js
@@ -0,0 +1,30 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import "./map.css";
+const location = {
+  address: "1600 Amphitheatre Parkway, Mountain View, california.",
+  lat: 37.42216,
+  lng: -122.08427,
+};
+
+const Map = ({ location, zoomLevel }) => (
+  <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={process.env.REACT_APP_KEY}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
+      </GoogleMapReact>
+    </div>
+  </div>
+);
+
+export default Map;
